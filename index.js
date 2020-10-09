2c08224234bfe54fcf0f1226edd2a829afca8fa8
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-09 14:10:25 -0400

    Add template questions

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of this project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe this project:',
+  },
+  {
+    type: 'input',
+    name: 'instalation',
+    message: 'How do you install this project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license does this project fall under?',
+    choices: [
+      'License 1',
+      'License 2',
+      'License 3',
+      ],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How may others contribute to this project?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are some tests for this project?',
+  },
+  {
+    type: 'input',
+    name: 'userName',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+  },
 ];
 
 // function to write README file
