6969a9453948f095cd57e392131b2785fc5b4ecc
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-12 12:51:51 -0400

    Modify generateMarkdown string to add full README template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,35 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+[![${data.license}](https://img.shields.io/badge/license-${data.license}-brightGreen)](https://choosealicense.com/licenses/mit/)
 
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under ${data.license}. For more information, see {link to license}.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.test}
+
+## Questions
+For additional questions, feel free to [send me an email](mailto:${data.email}). You can also find more information on my [GitHub profile](https://github.com/${data.userName}).
 `;
 }
 
